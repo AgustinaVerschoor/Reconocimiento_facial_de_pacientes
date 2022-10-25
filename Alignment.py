fbb19bf4f4e443da78ea7eff3168c1c0d5429c94
AgustinaVerschoor <agustina.verschoor@ing.austral.edu.ar>
2022-10-25 18:56:46 -0300

    Alignment 2.0 - ojos y circulos + lados del triangulo

--- Alignment.py
+++ Alignment.py
@@ -31,6 +31,11 @@ with mp_face_detection.FaceDetection(min_detection_confidence=0.5) as face_detec
                 cv2.putText(frame, "Ojo1", (x1 - 60, y1), 1, 1.5, (0, 255, 0), 2)
                 cv2.putText(frame, "Ojo2", (x2 + 10, y2), 1, 1.5, (0, 128, 255), 2)
 
+                #Lados del triangulo
+                cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.line(frame, (x1, y1), (x2, y1), (211, 0, 148), 2)
+                cv2.line(frame, (x2, y2), (x2, y1), (0, 128, 255), 2)
+
                 #Circulos en cada uno de los vertices del triangulo
                 cv2.circle(frame, (x1, y1), 5, (0, 255, 0), -1)
                 cv2.circle(frame, (x2, y2), 5, (0, 128, 255), -1)
