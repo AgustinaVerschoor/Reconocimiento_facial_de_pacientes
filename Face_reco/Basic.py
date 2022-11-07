39b7f77acb7ada28984ed289094d68557ee8b209
AgustinaVerschoor <agustina.verschoor@ing.austral.edu.ar>
2022-11-07 14:27:44 -0300

    Listo_presentar

--- Face_reco/Basic.py
+++
@@ -1,5 +0,0 @@
-#import face_recognition
-
-# problemas:
-# 1. no me aparece version de pyhton (como si no estuviera instalado)
-# 1. no me deja descargar dlib --> dependencia de face face_recognition. --> video con solucion.
--- MediaPipe_model/ArcFaceExample.py
+++
@@ -1,4 +0,0 @@
-from arcface import ArcFace
-face_rec = ArcFace.ArcFace()
-emb1 = face_rec.calc_emb("rostro_3.jpg")
-print(emb1)
